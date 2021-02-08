c58388623dead7aca4ddf03b14ce79495082a864
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-02-08 10:40:42 +0100

    changed visibilty from testing class

--- src/eu/imninja/Tests/ManagerTest.java
+++ src/eu/imninja/Tests/ManagerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ManagerTest {
+class ManagerTest {
 
     private TranslationManager translationManager;
     private ISO iso = new ISO(getClass().getResource("./config.properties").getPath(),"de");
